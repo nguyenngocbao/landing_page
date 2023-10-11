e7822bb805e1a5ddb8ba7f8c96994bbdd822cc44
Bao Nguyen <baonn2@vng.com.vn>
2023-10-11 14:55:35 +0700

    fix layout

--- components/container.js
+++ components/container.js
@@ -3,7 +3,7 @@ import React from "react";
 const Container = (props) => {
   return (
     <div
-      className={`container p-8 mx-auto lg:min-l-40 lg:min-r-40 ${
+      className={`container p-8 mx-auto xl:px-32 ${
         props.className ? props.className : ""
       }`}>
       {props.children}
--- components/navbar.js
+++ components/navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
 
   return (
     <div className="w-full">
-      <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between lg:min-l-40 lg:min-r-40">
+      <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-32">
         {/* Logo  */}
         <Disclosure>
           {({ open }) => (
