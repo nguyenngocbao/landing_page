0a3e86ebe1a9013a5fbb297257fc5b4473a6bbd5
Bao Nguyen <baonn2@vng.com.vn>
2023-10-23 15:57:48 +0700

    remove readme

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 const helloAPI = (req, res) =>{
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'Steve' })
 }
 
 export default helloAPI;
